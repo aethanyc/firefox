b173c32f62a7ed2df91843fc1f72e539fcec86bb
Ting-Yu Lin <aethanyc@gmail.com>
2025-05-21 11:45:36 -0700

    Use GridLineEdge() helper nsGridContainerFrame::ComputeIntrinsicISize().
    When computing content-box sizes for subgrid scenarios, call `GridLineEdge()` to
    align with other usages in the code, such as [1] and [2].
    
    This patch does not change behavior.
    
    [1] https://searchfox.org/mozilla-central/rev/dbef1a2f75798fb0136b7428d959c8feb09ad5d1/layout/generic/nsGridContainerFrame.cpp#9212-9213
    [2] https://searchfox.org/mozilla-central/rev/dbef1a2f75798fb0136b7428d959c8feb09ad5d1/layout/generic/nsGridContainerFrame.cpp#9161-9162

--- layout/generic/nsGridContainerFrame.cpp
+++ layout/generic/nsGridContainerFrame.cpp
@@ -10087,8 +10087,8 @@ nscoord nsGridContainerFrame::ComputeIntrinsicISize(
   if (MOZ_LIKELY(!IsSubgrid())) {
     return gridRI.mCols.SumOfGridTracksAndGaps();
   }
-  const auto& last = gridRI.mCols.mSizes.LastElement();
-  return last.mPosition + last.mBase;
+  return gridRI.mCols.GridLineEdge(gridRI.mCols.mSizes.Length(),
+                                   GridLineSide::BeforeGridGap);
 }
 
 nscoord nsGridContainerFrame::IntrinsicISize(const IntrinsicSizeInput& aInput,
