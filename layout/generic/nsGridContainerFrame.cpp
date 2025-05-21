93383717cfe57c340faea579d0ab571d2a79dcb0
Ting-Yu Lin <aethanyc@gmail.com>
2025-05-21 16:20:08 -0700

    Bug 1968137 Part 4 - Add a helper function to compute total track size without alignment. r?#layout-grid-reviewers
    The helper encapsulates a common pattern.
    
    This patch does not change behavior.
    
    Differential Revision: https://phabricator.services.mozilla.com/D250866

--- layout/generic/nsGridContainerFrame.cpp
+++ layout/generic/nsGridContainerFrame.cpp
@@ -2975,6 +2975,16 @@ struct nsGridContainerFrame::Tracks {
                            WritingMode aWM, nscoord aContentBoxSize,
                            bool aIsSubgridded);
 
+  /**
+   * Return the total resolved track size, ignoring any alignment adjustments
+   * that AlignJustifyContent() might apply.
+   *
+   * Note: This method assumes that AlignJustifyContent() might not be called
+   * yet.
+   */
+  nscoord TotalTrackSizeWithoutAlignment(
+      const nsGridContainerFrame* aGridContainerFrame) const;
+
   nscoord GridLineEdge(uint32_t aLine, GridLineSide aSide) const {
     if (MOZ_UNLIKELY(mSizes.IsEmpty())) {
       // https://drafts.csswg.org/css-grid-2/#grid-definition
@@ -7584,6 +7594,18 @@ void nsGridContainerFrame::Tracks::AlignJustifyContent(
   MOZ_ASSERT(!roundingError, "we didn't distribute all rounding error?");
 }
 
+nscoord nsGridContainerFrame::Tracks::TotalTrackSizeWithoutAlignment(
+    const nsGridContainerFrame* aGridContainerFrame) const {
+  if (aGridContainerFrame->IsSubgrid(mAxis)) {
+    return GridLineEdge(mSizes.Length(), GridLineSide::BeforeGridGap);
+  }
+
+  // This method assumes that AlignJustifyContent() might not be called yet.
+  // Therefore, we can't use GridLineEdge() here, as mPosition may not be
+  // calculated.
+  return SumOfGridTracksAndGaps();
+}
+
 void nsGridContainerFrame::LineRange::ToPositionAndLength(
     const nsTArray<TrackSize>& aTrackSizes, nscoord* aPos,
     nscoord* aLength) const {
@@ -9215,17 +9237,8 @@ nscoord nsGridContainerFrame::ComputeBSizeForResolvingRowSizes(
                                      NS_UNCONSTRAINEDSIZE,
                                      SizingConstraint::NoConstraint);
 
-  nscoord result;
-  if (!IsRowSubgrid()) {
-    // Note: we can't use GridLineEdge here since we haven't calculated the
-    // rows' mPosition yet (happens in a later AlignJustifyContent call in
-    // Reflow()).
-    result = aGridRI.mRows.SumOfGridTracksAndGaps();
-  } else {
-    result = aGridRI.mRows.GridLineEdge(aGridRI.mRows.mSizes.Length(),
-                                        GridLineSide::BeforeGridGap);
-  }
-  result = aGridRI.mReflowInput->ApplyMinMaxBSize(result);
+  const nscoord result = aGridRI.mReflowInput->ApplyMinMaxBSize(
+      aGridRI.mRows.TotalTrackSizeWithoutAlignment(this));
 
   // Invalidate the row sizes before re-resolving them in Reflow().
   aGridRI.InvalidateTrackSizesForAxis(LogicalAxis::Block);
@@ -9262,16 +9275,7 @@ nscoord nsGridContainerFrame::ComputeIntrinsicContentBSize(
     return aBSizeForResolvingRowSizes;
   }
 
-  // Use the resolved sizes of our rows.
-  if (!IsRowSubgrid()) {
-    // Note: we can't use GridLineEdge here since we haven't calculated
-    // the rows' mPosition yet (happens in a later AlignJustifyContent call in
-    // Reflow()).
-    return aGridRI.mRows.SumOfGridTracksAndGaps();
-  }
-
-  const uint32_t numRows = aGridRI.mRows.mSizes.Length();
-  return aGridRI.mRows.GridLineEdge(numRows, GridLineSide::BeforeGridGap);
+  return aGridRI.mRows.TotalTrackSizeWithoutAlignment(this);
 }
 
 void nsGridContainerFrame::Reflow(nsPresContext* aPresContext,
@@ -10093,11 +10097,7 @@ nscoord nsGridContainerFrame::ComputeIntrinsicISize(
                                       NS_UNCONSTRAINEDSIZE, constraint);
   }
 
-  if (MOZ_LIKELY(!IsSubgrid())) {
-    return gridRI.mCols.SumOfGridTracksAndGaps();
-  }
-  return gridRI.mCols.GridLineEdge(gridRI.mCols.mSizes.Length(),
-                                   GridLineSide::BeforeGridGap);
+  return gridRI.mCols.TotalTrackSizeWithoutAlignment(this);
 }
 
 nscoord nsGridContainerFrame::IntrinsicISize(const IntrinsicSizeInput& aInput,
