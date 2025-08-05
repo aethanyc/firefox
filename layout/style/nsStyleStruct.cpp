c49522d86115ada4fd3b10a841f184a1319a3299
Ting-Yu Lin <tlin@mozilla.com>
2025-08-05 16:45:12 -0700

    Bug 1980104 - Add style system support for the text-autospace property.
    Differential Revision: https://phabricator.services.mozilla.com/D259303

--- layout/style/nsStyleStruct.cpp
+++ layout/style/nsStyleStruct.cpp
@@ -3037,7 +3037,8 @@ nsStyleText::nsStyleText(const nsStyleText& aSource)
       mHyphenateCharacter(aSource.mHyphenateCharacter),
       mHyphenateLimitChars(aSource.mHyphenateLimitChars),
       mWebkitTextSecurity(aSource.mWebkitTextSecurity),
-      mTextWrapStyle(aSource.mTextWrapStyle) {
+      mTextWrapStyle(aSource.mTextWrapStyle),
+      mTextAutospace(aSource.mTextAutospace) {
   MOZ_COUNT_CTOR(nsStyleText);
 }
 
@@ -3073,7 +3074,8 @@ nsChangeHint nsStyleText::CalcDifference(const nsStyleText& aNewData) const {
       (mHyphenateCharacter != aNewData.mHyphenateCharacter) ||
       (mHyphenateLimitChars != aNewData.mHyphenateLimitChars) ||
       (mWebkitTextSecurity != aNewData.mWebkitTextSecurity) ||
-      (mTextWrapStyle != aNewData.mTextWrapStyle)) {
+      (mTextWrapStyle != aNewData.mTextWrapStyle) ||
+      (mTextAutospace != aNewData.mTextAutospace)) {
     return NS_STYLE_HINT_REFLOW;
   }
 
