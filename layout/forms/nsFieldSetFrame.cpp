8fda4f88e63549757078893f243a3a1ed4ae8b31
Ting-Yu Lin <tlin@mozilla.com>
2025-10-21 10:30:44 -0700

    Bug 1994083 - Generate display items for frames in absolute child list.

--- layout/forms/nsFieldSetFrame.cpp
+++ layout/forms/nsFieldSetFrame.cpp
@@ -229,6 +229,7 @@ void nsFieldSetFrame::BuildDisplayList(nsDisplayListBuilder* aBuilder,
 
   if (GetPrevInFlow()) {
     DisplayOverflowContainers(aBuilder, aLists);
+    DisplayAbsoluteFrames(aBuilder, aLists);
   }
 
   nsDisplayListCollection contentDisplayItems(aBuilder);
