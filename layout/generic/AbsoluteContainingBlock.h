1268cb3b66ccad23073e71b35e4723a58b7cd6a5
Ting-Yu Lin <tlin@mozilla.com>
2025-11-05 11:07:49 -0800

    Change aDelegatingFrame type to nsContainerFrame* for ReflowAbsoluteFrame().
    The `aDelegatingFrame` parameter on `AbsoluteContainingBlock::Reflow()` is
    already `nsContainerFrame*`.

--- layout/generic/AbsoluteContainingBlock.h
+++ layout/generic/AbsoluteContainingBlock.h
@@ -146,7 +146,7 @@ class AbsoluteContainingBlock {
                                      LogicalMargin& aOffsets);
 
   void ReflowAbsoluteFrame(
-      nsIFrame* aDelegatingFrame, nsPresContext* aPresContext,
+      nsContainerFrame* aDelegatingFrame, nsPresContext* aPresContext,
       const ReflowInput& aReflowInput,
       const nsRect& aOriginalContainingBlockRect, AbsPosReflowFlags aFlags,
       nsIFrame* aKidFrame, nsReflowStatus& aStatus,
