d7ad538168777c6606f62316e50e7ec2be326e72
Ting-Yu Lin <tlin@mozilla.com>
2025-12-08 13:36:46 -0800

    Add a frame bit to mark pushed abspos frames for painting.

--- layout/generic/nsContainerFrame.cpp
+++ layout/generic/nsContainerFrame.cpp
@@ -1018,10 +1018,10 @@ void nsContainerFrame::DisplayOverflowContainers(
   }
 }
 
-void nsContainerFrame::DisplayAbsoluteContinuations(
+void nsContainerFrame::DisplayPushedAbsoluteFrames(
     nsDisplayListBuilder* aBuilder, const nsDisplayListSet& aLists) {
   for (nsIFrame* frame : GetChildList(FrameChildListID::Absolute)) {
-    if (frame->GetPrevInFlow()) {
+    if (frame->HasAnyStateBits(NS_FRAME_IS_PUSHED_ABSPOS)) {
       BuildDisplayListForChild(aBuilder, frame, aLists);
     }
   }
