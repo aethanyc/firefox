51e1e777f86ab82916b23cdd113dc7cddda0251d
Ting-Yu Lin <tlin@mozilla.com>
2026-02-12 16:15:52 -0800

    Bug 2009645 - Allow zero-height positioned element placed at the end of a fragmentainer.
    In this patch, we allow a positioned element to reflow under zero available
    block-size if its position is exactly at the block-end edge of the current
    fragmentainer.

--- layout/generic/AbsoluteContainingBlock.cpp
+++ layout/generic/AbsoluteContainingBlock.cpp
@@ -737,7 +737,7 @@ void AbsoluteContainingBlock::Reflow(nsContainerFrame* aDelegatingFrame,
         // fragmentainer, push it to the next fragmentainer.
         const nscoord kidBPosInThisFragment =
             unfragPos->B(containerWM) - mCumulativeContainingBlockBSize;
-        if (kidBPosInThisFragment >= availBSize) {
+        if (kidBPosInThisFragment > availBSize) {
           kidFrameNeedsPush = true;
         }
       }
@@ -1778,7 +1778,7 @@ void AbsoluteContainingBlock::ReflowAbsoluteFrame(
         const nscoord kidBPosInThisFragment =
             unfragmentedPosition->B(outerWM) - mCumulativeContainingBlockBSize;
         availBSize = aReflowInput.AvailableBSize() - kidBPosInThisFragment;
-        NS_ASSERTION(availBSize > 0, "Why is available block-size <= 0?");
+        NS_ASSERTION(availBSize >= 0, "Why is available block-size < 0?");
       } else if (!aDelegatingFrame->GetPrevInFlow()) {
         // aDelegatingFrame is a first-in-flow. We subtract our containing
         // block's border-block-start, to consider the available space as
