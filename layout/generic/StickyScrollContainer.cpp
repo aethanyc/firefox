9695f8e17de589525434e218aa363493f42e36d4
Ting-Yu Lin <tlin@mozilla.com>
2026-09-03 16:04:51 -0700

    Bug 2057630 Part 3 - Add two helpers in StickyScrollContainer. r?#layout
    `ComputeTranslationIgnoringScrolling()` is required for the next part.
    
    Differential Revision: https://phabricator.services.mozilla.com/D324788

--- layout/generic/StickyScrollContainer.cpp
+++ layout/generic/StickyScrollContainer.cpp
@@ -259,7 +259,17 @@ StickyScrollContainer::StickyLimits StickyScrollContainer::ComputeStickyLimits(
 }
 
 nsPoint StickyScrollContainer::ComputePosition(nsIFrame* aFrame) const {
-  const auto [stick, contain] = ComputeStickyLimits(aFrame);
+  return DoComputePosition(aFrame, StickyLimitSpace::RelativeToCurrentScroll);
+}
+
+nsPoint StickyScrollContainer::ComputePositionIgnoringScrolling(
+    nsIFrame* aFrame) const {
+  return DoComputePosition(aFrame, StickyLimitSpace::IgnoreCurrentScroll);
+}
+
+nsPoint StickyScrollContainer::DoComputePosition(
+    nsIFrame* aFrame, StickyLimitSpace aSpace) const {
+  const auto [stick, contain] = ComputeStickyLimits(aFrame, aSpace);
 
   nsPoint position = aFrame->GetNormalPosition();
 
@@ -272,6 +282,12 @@ nsPoint StickyScrollContainer::ComputePosition(nsIFrame* aFrame) const {
   position.x = std::min(position.x, std::max(stick.XMost(), contain.x));
 
   return position;
+}
+
+nsPoint StickyScrollContainer::ComputeTranslationIgnoringScrolling(
+    const nsIFrame* aFrame) const {
+  nsIFrame* first = nsLayoutUtils::FirstContinuationOrIBSplitSibling(aFrame);
+  return ComputePositionIgnoringScrolling(first) - first->GetNormalPosition();
 }
 
 bool StickyScrollContainer::IsStuckInYDirection(nsIFrame* aFrame) const {
