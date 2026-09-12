db84dcb8740fbecfb0acd6077863bbd22c614b16
Ting-Yu Lin <tlin@mozilla.com>
2026-09-12 13:11:45 -0700

    Bug 2072038 - Simplify nsIFrame::GetNormalRect(). r?#layout
    `GetNormalPosition()` already falls back to `GetPosition()` when the
    frame has no `NormalPositionProperty`.
    
    This patch doesn't change behavior.
    
    Differential Revision: https://phabricator.services.mozilla.com/D326023

--- layout/generic/nsIFrame.cpp
+++ layout/generic/nsIFrame.cpp
@@ -8677,12 +8677,7 @@ void nsIFrame::MovePositionBy(const nsPoint& aTranslation) {
 }
 
 nsRect nsIFrame::GetNormalRect() const {
-  bool hasProperty;
-  nsPoint normalPosition = GetProperty(NormalPositionProperty(), &hasProperty);
-  if (hasProperty) {
-    return nsRect(normalPosition, GetSize());
-  }
-  return GetRect();
+  return nsRect(GetNormalPosition(), GetSize());
 }
 
 nsRect nsIFrame::GetBoundingClientRect() {
